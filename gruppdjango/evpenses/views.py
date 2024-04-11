9fe3b128e66c115a80548f573de7e28e8514e828
frederikengmark <frederik.engmark@edu.vellinge.se>
2024-04-11 09:55:38 +0200

    6

--- gruppdjango/evpenses/views.py
+++ gruppdjango/evpenses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from datetime import datetime
+x=datetime.now
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html', {'date': x})
